fd2f7eedefecf9d8467ee731698e23d079f0ec13
Remy Wang <remywang@cs.washington.edu>
2025-12-04 00:12:47 -0800

    cuniform light heavy

--- Source/main.lua
+++ Source/main.lua
@@ -6,7 +6,7 @@ local gfx = playdate.graphics
 local geo = playdate.geometry
 local net = playdate.network
 
-local fnt = gfx.font.new("fonts/Cuniform")
+local fnt = gfx.font.new("fonts/cuniform-heavy")
 gfx.setFont(fnt)
 
 local directory = "https://orbit.casa/directory.md"
@@ -172,7 +172,7 @@ end
 local page = initializePage()
 
 -- Initialize C renderer (font cache only)
-cmark.initRenderer("fonts/Cuniform")
+cmark.initRenderer("fonts/cuniform-heavy")
 
 -- Links array (populated by render())
 local links = {}
